6c8b67e2462e6c4b4fd6be277049d41682645ce5
bassixs <khiltor@bk.ru>
2025-07-16 17:14:40 +0300

    вав

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tiffany: '#81D8D0',
+        pink: '#FF69B4',
+      },
+    },
   },
   plugins: [],
-};
+}
