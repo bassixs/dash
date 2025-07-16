d04d77b2a4e52a1e78273c9558fde27a86659fcd
bassixs <khiltor@bk.ru>
2025-07-16 17:00:26 +0300

    jujuj

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,8 @@ import tailwindcss from '@tailwindcss/postcss';
 import autoprefixer from 'autoprefixer';
 
 export default {
-  plugins: {
-    tailwindcss: {}, // ✅ Так нужно!
-    autoprefixer: {},
-  },
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx}',
-  ],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {
-      colors: {
-        tiffany: '#81D8D0',
-        pink: '#FF69B4',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
